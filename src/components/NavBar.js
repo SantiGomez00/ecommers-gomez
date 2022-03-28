52e64b906051f01df78ca25c713168265f4ff519
santiago gomez <gomezsg00@gmail.com>
2022-03-28 19:50:22 -0300

    version 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav id="navBar">
+            <div id="nombre">Papelera</div>
+            <div id="botones">
+                <a href='#navBar'>tienda</a>
+                <a href='#navBar'>carrito</a>
+                <a href='#navBar'>info</a>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
