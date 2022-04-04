af7fc3888d1037bb67708f65caf318ca3c0080b1
santiago gomez <gomezsg00@gmail.com>
2022-04-04 19:33:40 -0300

    version 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import Contador from './components/ItemCount'
+
 
 function App() {
+  const add = (cantidad) =>{
+  console.log("se agrego " +cantidad +" al carrito")
+}
   return (
     <div className="App">
       <NavBar></NavBar>
       <ItemListContainer></ItemListContainer>
+      <Contador initial={1}stock={10}add={add}></Contador>
     </div>
   );
 }
---
+++ src/components/ItemCount.js
@@ -0,0 +1,39 @@
+import {useState,useEffect} from "react";
+import "./NavBar.css"
+
+const Contador = ({initial,stock,add}) =>{
+    const [count,setCount] = useState (initial)
+    useEffect (() =>{
+        console.log("funciona")
+    },[])
+    const sumar = () => {
+        if ( count >= stock ) {
+            console.log("no se puede agregar mas :(")
+        }
+        else {
+            setCount (count +1)
+        }
+    }
+    const restar = () => {
+        if (count <= 1 ){
+            console.log ("no puedes comprar en negativo :)")
+        }
+        else {
+            setCount (count -1)
+        }
+    }
+    return (
+        <div>
+            <div id="counter">
+                <button id="botonMenos" onClick={restar}> - </button>
+                <p id="cantidad">{count}</p>
+                <button id="botonMas" onClick={sumar}> + </button> 
+                <div>stock = {stock}</div>
+            </div>
+            <button id="addCarrito" onClick={()=>add(count)}>agregar al carrito</button>
+            
+        </div>
+    )
+}
+
+export default Contador
