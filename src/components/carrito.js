61169a523a5d80b2afaab4ef4d1fa2a7b1839e54
santiago gomez <gomezsg00@gmail.com>
2022-05-02 21:53:03 -0300

    v14

--- src/components/carrito.js
+++ src/components/carrito.js
@@ -1,11 +1,52 @@
 import CartContext from "./cartContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import {writeBatch, collection, getDocs, query, where, documentId, addDoc} from "firebase/firestore";
+import { firestoreDb } from "../services/firebase";
 
 const Carrito = () => {
     const {carrito} = useContext (CartContext)
     const {eliminarItem} = useContext (CartContext)
     const {vaciarCarrito} = useContext (CartContext)
+
+    const ordenCompra = () => {
+        const orden = {
+            item: carrito,
+            comprador: {
+                nombre: "juan",
+                telefono: 12345678,
+                email: "juan@mail.com"
+            },
+            total: precioFinal,
+            date: new Date()
+        }
+        const idProductosComprar = carrito.map(prod=>prod.id)
+        const actualizar = writeBatch(firestoreDb)
+        const collectionRef = collection(firestoreDb,"productos")
+        const sinStock = []
+        getDocs (query(collectionRef,where(documentId(), "in", idProductosComprar))).then(response => {
+            response.docs.forEach(doc => {
+                const dataDoc = doc.data()
+                const cantidadProducto = carrito.find (prod => prod.id === doc.id)?.quantity
+                if(dataDoc.stock >= cantidadProducto){
+                    actualizar.update(doc.ref,{stock: dataDoc.stock - cantidadProducto})
+                } else{
+                    sinStock.push({id:doc.id,...dataDoc})
+                }
+            })
+        }).then(() => {
+            if(sinStock.length === 0) {
+                const collectionRef = collection (firestoreDb, "orders")
+                return addDoc (collectionRef,orden)
+            }
+            else{
+                return Promise.reject ({name:"stock error", productos: sinStock})
+            }
+        }).then((id) => {
+            actualizar.commit()
+            vaciarCarrito()
+        }).catch(error=>console.log(error))
+    }
 
     let precioFinal = 0
     
@@ -42,6 +83,9 @@ const Carrito = () => {
                     total: $ {precioFinal}
                     <button onClick={vaciarCarrito}>
                         vaciar carrito
+                    </button>
+                    <button onClick={ordenCompra}>
+                        comprar
                     </button>
                 </div>
             } 
